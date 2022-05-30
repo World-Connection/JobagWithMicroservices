e50c55dfacd9422e03b293cdf6f0c4afba50a7b6
bdvela <bdvela20@gmail.com>
2022-05-30 01:04:26 -0500

    fix: errors in controllers

--- src/main/java/upc/edu/pe/postulantmicroservice/controller/CVController.java
+++ src/main/java/upc/edu/pe/postulantmicroservice/controller/CVController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import upc.edu.pe.postulantmicroservice.entity.CV;
+
 import upc.edu.pe.postulantmicroservice.services.CVService;
 
 import javax.validation.Valid;
@@ -25,70 +26,34 @@ public class CVController {
     private CVService cvService;
 
     @GetMapping
-    public ResponseEntity<List<CV>> listCV(){
-        List<CV> cvs = new ArrayList<>();
-        cvs = cvService.findAllCV();
-        if(cvs.isEmpty()){
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.ok(cvs);
+    public ResponseEntity<List<CV>> getAllCV() {
+        List<CV> cvList = cvService.findAllCV();
+        if(cvList == null) return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(cvList);
     }
-
     @GetMapping(value = "/{id}")
-    public ResponseEntity<CV> getCV(@PathVariable("id") Long id){
+    public ResponseEntity<CV> getCV(@PathVariable("id")Long id) {
         CV cv = cvService.getCV(id);
-        if(cv == null){
-            return ResponseEntity.notFound().build();
-        }
+        if(cv == null) return ResponseEntity.noContent().build();
         return ResponseEntity.ok(cv);
     }
-
     @PostMapping
-    public ResponseEntity<CV> createCV(@Valid @RequestBody CV cv, BindingResult result){
-        if(result.hasErrors()){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, this.formatMessage(result));
-        }
-        CV cvCreate = cvService.createCV(cv);
-        return ResponseEntity.status(HttpStatus.CREATED).body(cvCreate);
+    public ResponseEntity<CV> createCV(@RequestBody CV cv) {
+        if(cv == null) return ResponseEntity.noContent().build();
+        cvService.createCV(cv);
+        return ResponseEntity.status(HttpStatus.CREATED).body(cv);
     }
-
     @PutMapping(value = "/{id}")
-    public ResponseEntity<CV> updateCV(@PathVariable("id") Long id, @RequestBody CV cv){
+    public ResponseEntity<CV> updateCV(@PathVariable("id")Long id, @RequestBody CV cv){
+        if(id == null || cv == null) return ResponseEntity.noContent().build();
         cv.setId(id);
-        CV cvDB = cvService.updateCV(cv);
-        if( cvDB == null){
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(cvDB);
+        cvService.updateCV(cv);
+        return ResponseEntity.ok(cv);
     }
-
-    @DeleteMapping(value = "{id}")
-    public ResponseEntity<CV> deleteCV(@PathVariable("id") Long id){
-        CV cvDelete = cvService.deleteCV(id);
-        if(cvDelete == null){
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(cvDelete);
-    }
-
-    private String formatMessage( BindingResult result){
-        List<Map<String,String>> errors = result.getFieldErrors().stream()
-                .map(err ->{
-                    Map<String,String>  error =  new HashMap<>();
-                    error.put(err.getField(), err.getDefaultMessage());
-                    return error;
-
-                }).collect(Collectors.toList());
-        ErrorMessage errorMessage = ErrorMessage.builder()
-                .code("01")
-                .messages(errors).build();
-        ObjectMapper mapper = new ObjectMapper();
-        String jsonString="";
-        try {
-            jsonString = mapper.writeValueAsString(errorMessage);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return jsonString;
+    @DeleteMapping(value = "/{id}")
+    public void deleteCV(@PathVariable Long id) {
+        CV cv = cvService.getCV(id);
+        if(cv == null) return;
+        cvService.deleteCV(id);
     }
 }
--- src/main/java/upc/edu/pe/postulantmicroservice/controller/StudyController.java
+++ src/main/java/upc/edu/pe/postulantmicroservice/controller/StudyController.java
@@ -43,3 +43,5 @@ public class StudyController {
         studyService.deleteStudy(id);
     }
 }
+
+
